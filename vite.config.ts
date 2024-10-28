6c5cf0cc8be3313797f5ce7a94f5dd2e0388059b
João Lameira <jmrlameira@gmail.com>
2024-10-28 03:39:29 +0000

    add proxy url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd())
+export default defineConfig(() => {
 	return {
 		plugins: [react()],
 		base: '/fruity-vice-app/',
@@ -19,13 +18,6 @@ export default defineConfig(({ mode }) => {
 			watch: {
 				usePolling: true
 			}
-			/* 			proxy: {
-				'/api': {
-					target: env.VITE_API_URL,
-					changeOrigin: true,
-					rewrite: path => path.replace(/^\/api/, '')
-				}
-			} */
 		}
 	}
 })
