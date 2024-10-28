d50adec2b04c580f313d93a66ac5f4c86c591a89
João Lameira <jmrlameira@gmail.com>
2024-10-28 03:50:52 +0000

    add api url

--- src/services/fruits.ts
+++ src/services/fruits.ts
@@ -2,7 +2,7 @@ import { Fruit } from '@/types'
 
 export const getFruits = async (): Promise<Fruit[] | undefined> => {
 	try {
-		const response = await fetch('/api')
+		const response = await fetch('https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws')
 
 		if (!response.ok) throw new Error('Network response was not ok')
 
